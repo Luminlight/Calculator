32bcaab9fe08f2a0fe3a8d97b82e00e0873f3874
Luminlight <razznikx@gmail.com>
2022-07-22 15:56:39 -0400

    Added an additional display output used for result. Wrapped outputs in their own div.

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
+/* DOM elements */
 const numberButtons = document.querySelectorAll(".number-button");
 const operatorButtons = document.querySelectorAll(".operator-button");
 const decimalButton = document.querySelector(".decimal-button");
 const equalButton = document.querySelector(".equal-button");
 const clearButton = document.querySelector(".clear-button");
 const deleteButton = document.querySelector(".delete-button");
-const displayNumber = document.querySelector(".display");
+const inputDisplayNumber = document.querySelector(".input-display");
+const resultDisplayNumber = document.querySelector(".result-display");
 
-// Event listeners
+/*  Event listeners */
 numberButtons.forEach((btn) =>
     btn.addEventListener("click", () => appendNumberScreen(btn.textContent))
 );
@@ -19,35 +21,38 @@ clearButton.addEventListener("click", clearDisplay);
 deleteButton.addEventListener("click", deleteNumber);
 decimalButton.addEventListener("click", appendDecimal);
 equalButton.addEventListener("click", () => {
-    displayNumber.textContent = displayNumber.textContent;
+    inputDisplayNumber.textContent = inputDisplayNumber.textContent;
     // Probably need to have/add another display
     // Pressing this button displays a "new" number
 });
 
-// Functions
+/* Functions */
 function appendNumberScreen(number) {
-    if (displayNumber.textContent === "0") {
-        displayNumber.textContent = ""; // Removes leading 0
+    if (inputDisplayNumber.textContent === "0") {
+        inputDisplayNumber.textContent = ""; // Removes leading 0
     }
-    displayNumber.textContent += number;
+    inputDisplayNumber.textContent += number;
 }
 
 function clearDisplay() {
-    displayNumber.textContent = "0";
+    inputDisplayNumber.textContent = "0";
 }
 
 function deleteNumber() {
-    displayNumber.textContent = displayNumber.textContent.slice(0, -1);
-    if (displayNumber.textContent.length === 0) {
+    inputDisplayNumber.textContent = inputDisplayNumber.textContent.slice(
+        0,
+        -1
+    );
+    if (inputDisplayNumber.textContent.length === 0) {
         clearDisplay(); // If all values are deleted, value is set to 0
     }
 }
 
 function appendDecimal() {
-    if (displayNumber.textContent.includes(".")) {
+    if (inputDisplayNumber.textContent.includes(".")) {
         return; // Prevents multiple being added
     }
-    displayNumber.textContent += ".";
+    inputDisplayNumber.textContent += ".";
 }
 
 const add = (a, b) => {
